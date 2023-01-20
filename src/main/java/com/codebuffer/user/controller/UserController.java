e143234b5138239c55fd7e7f66b4edb3d93a563e
Kelvin Dzikiti <kieva290@gmail.com>
2023-01-20 18:16:38 +0200

    first commit

---
+++ src/main/java/com/codebuffer/user/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.codebuffer.user.controller;
+
+import com.codebuffer.user.VO.ResponseTemplateVO;
+import com.codebuffer.user.entity.User;
+import com.codebuffer.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user) {
+        log.info("Inside saveUser of user-controller");
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId) {
+        log.info("Inside getUserWithDepartment of user-controller");
+        return userService.getUserWithDepartment(userId);
+    }
+}
