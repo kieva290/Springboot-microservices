369b447ae0f8719ec4e980ab977031e9c1a92387
kieva290@gmail.com <kieva290@gmail.com>
2023-01-25 12:20:44 +0200

    forth commit

---
+++ user-service/src/main/java/com/codebuffer/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.codebuffer.user.repository;
+
+import com.codebuffer.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUserId(Long userId);
+}
